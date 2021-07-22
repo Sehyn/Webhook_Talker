fc5ff14629933898a79c3d2f0525cf3eaa00b7cb
Arthur.B <48888351+Sehyn@users.noreply.github.com>
2021-07-22 03:19:01 +0200

    Add files via upload

---
+++ WebhookTalker/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace WebhookTalker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.TxtWebhookurl = new System.Windows.Forms.TextBox();
+            this.TxtMessage = new System.Windows.Forms.TextBox();
+            this.BtnSend = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // TxtWebhookurl
+            // 
+            this.TxtWebhookurl.Location = new System.Drawing.Point(92, 10);
+            this.TxtWebhookurl.Name = "TxtWebhookurl";
+            this.TxtWebhookurl.Size = new System.Drawing.Size(248, 20);
+            this.TxtWebhookurl.TabIndex = 0;
+            // 
+            // TxtMessage
+            // 
+            this.TxtMessage.Location = new System.Drawing.Point(92, 36);
+            this.TxtMessage.Name = "TxtMessage";
+            this.TxtMessage.Size = new System.Drawing.Size(248, 20);
+            this.TxtMessage.TabIndex = 1;
+            // 
+            // BtnSend
+            // 
+            this.BtnSend.Location = new System.Drawing.Point(92, 62);
+            this.BtnSend.Name = "BtnSend";
+            this.BtnSend.Size = new System.Drawing.Size(248, 23);
+            this.BtnSend.TabIndex = 2;
+            this.BtnSend.Text = "Send";
+            this.BtnSend.UseVisualStyleBackColor = true;
+            this.BtnSend.Click += new System.EventHandler(this.BtnSend_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(4, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(82, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Webhook URL:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(33, 39);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Message:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(352, 103);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.BtnSend);
+            this.Controls.Add(this.TxtMessage);
+            this.Controls.Add(this.TxtWebhookurl);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Webhook Talker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox TxtWebhookurl;
+        private System.Windows.Forms.TextBox TxtMessage;
+        private System.Windows.Forms.Button BtnSend;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ WebhookTalker/Form1.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WebhookTalker
+{
+
+    public partial class Form1 : Form
+    {
+        private static string WebhookURL = "";
+        private static string Message = "";
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void BtnSend_Click(object sender, EventArgs e)
+        {
+            Message = TxtMessage.Text;
+            WebhookURL = TxtWebhookurl.Text;
+
+            Http.Post(WebhookURL, new NameValueCollection()
+            {
+                { "username", "FLUVE.ME | Beep Boop" },
+                { "avatar_url", "https://cdn.discordapp.com/attachments/696080024742395914/718483498947838063/beetlejuice-1.jpg" },
+                { "content", Message.ToString() }
+            });
+        }
+
+        class Http
+        {
+            public static byte[] Post(string uri, NameValueCollection pairs)
+            {
+                using (WebClient webClient = new WebClient())
+                    return webClient.UploadValues(uri, pairs);
+            }
+        }
+    }
+}
